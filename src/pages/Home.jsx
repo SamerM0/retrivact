3c25a887623179928d8a3a6f3ef0dee067a06656
Samer Mohamed <samer.mohamed600012@gmail.com>
2025-08-19 18:25:16 +0300

    add temp colors and home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex justify-center items-center'>
+        <h1 className='text-primary font-funnel text-5xl tracking-widest mt-30'>retrivact</h1>
+    </div>
+  )
+}
+
+export default Home
