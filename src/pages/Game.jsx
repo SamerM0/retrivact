3e51234699b0e209285b09a338f344f5c472e6e6
Samer Mohamed <samer.mohamed600012@gmail.com>
2025-08-21 20:18:08 +0300

    add a way to load new questions

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -9,29 +9,24 @@ function Game({ category, difficulty }) {
   const [isFirstLoading, setIsFirstLoading] = useState(true);
   function answer() {
     setQuestionIndex(questionIndex + 1);
-  };
+  }
+  async function loadQuestions() {
+    setIsLoading(true);
+    const data = await getData(10, category, difficulty);
+    console.log(data);
+    setData((prev) => [...prev, ...data]);
+    setIsLoading(false);
+  }
   useEffect(() => {
-    async function loadQuestions() {
-      setIsLoading(true);
-      const data = await getData(10, category, difficulty);
-      console.log(data);
-      setData((prev) => [...prev, ...data]);
-      setIsLoading(false);
+    if (isFirstLoading) {
+      loadQuestions();
+      setIsFirstLoading(false);
     }
-    loadQuestions();
-  }, []);
-  useEffect(() => {
-    if (isLoading) {
-      console.log("LOADING STARTED");
-    } else {
-      console.log("LOADING ENDED");
-
-      if (isFirstLoading) {
-        setIsFirstLoading(false);
-      }
+    if (questionIndex + 5 >= data.length && !isLoading) {
+      loadQuestions();
+      console.log("loading new questions");
     }
-  }, [isLoading]);
-  
+  }, [questionIndex]);
   return (
     <div>
       {!isFirstLoading && data[questionIndex] && (
